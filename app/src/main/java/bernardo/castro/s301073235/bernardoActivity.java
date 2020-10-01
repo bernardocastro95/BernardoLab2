23f666214b9dd63c5df64159872a6eed609f1959
Bernardo Castro <bernardoc544@gmail.com>
2020-10-01 16:22:05 -0400

    Options activity

--- app/src/main/java/bernardo/castro/s301073235/bernardoActivity.java
+++ app/src/main/java/bernardo/castro/s301073235/bernardoActivity.java
@@ -2,14 +2,25 @@ package bernardo.castro.s301073235;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class bernardoActivity extends AppCompatActivity {
 
+    Button b;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        b = findViewById(R.id.bernardoEnterButton);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(bernardoActivity.this, castroActivityOptions.class));
+            }
+        });
     }
     //Bernardo Silveira dos Santos Castro, StudentID: 301073235, Section 002
 }
---
+++ app/src/main/java/bernardo/castro/s301073235/castroActivityOptions.java
@@ -0,0 +1,15 @@
+package bernardo.castro.s301073235;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class castroActivityOptions extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_castro_options);
+        //Bernardo Silveira dos Santos Castro, StudentID: 301073235, Section 002
+    }
+}
