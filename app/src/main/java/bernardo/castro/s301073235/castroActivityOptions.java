87b1f1d09bf52a0aaee87ec2da822da34edc4f21
Bernardo Castro <bernardoc544@gmail.com>
2020-10-03 11:27:29 -0400

    Menu functionality implementated

--- app/src/main/java/bernardo/castro/s301073235/castroActivityOptions.java
+++ app/src/main/java/bernardo/castro/s301073235/castroActivityOptions.java
@@ -1,10 +1,13 @@
 package bernardo.castro.s301073235;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class castroActivityOptions extends AppCompatActivity {
 
@@ -20,5 +23,25 @@ public class castroActivityOptions extends AppCompatActivity {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.menu_options, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.bernardoApartment:
+                Toast.makeText(this, "Apartment", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.bernardoDetachedHome:
+                return true;
+            case R.id.bernardoSemiDetachedHome:
+                return true;
+            case R.id.bernardoCondominium:
+                return true;
+            case R.id.bernardoTownHouse:
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
     }
 }
