1412efd68c9f44c68c786214f21db65db2ce2b1d
Bernardo Castro <bernardoc544@gmail.com>
2020-10-02 20:26:34 -0400

    Landscape mode for second activity and options menu

--- app/src/main/java/bernardo/castro/s301073235/castroActivityOptions.java
+++ app/src/main/java/bernardo/castro/s301073235/castroActivityOptions.java
@@ -3,6 +3,8 @@ package bernardo.castro.s301073235;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class castroActivityOptions extends AppCompatActivity {
 
@@ -11,5 +13,12 @@ public class castroActivityOptions extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_castro_options);
         //Bernardo Silveira dos Santos Castro, StudentID: 301073235, Section 002
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_options, menu);
+        return true;
     }
 }
