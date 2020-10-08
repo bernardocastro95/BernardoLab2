8ab4fe0c123c4a1f191ef96b800692104103bd20
Bernardo Castro <bernardoc544@gmail.com>
2020-10-08 11:24:39 -0400

    Card form activity and radio button functionality

---
+++ app/src/main/java/bernardo/castro/s301073235/castroActivityCardForm.java
@@ -0,0 +1,14 @@
+package bernardo.castro.s301073235;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+//Bernardo Silveira dos Santos Castro, StudentID: 301073235, Section 002
+public class castroActivityCardForm extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.castro_activity_card_form);
+    }
+}
--- app/src/main/java/bernardo/castro/s301073235/castroActivityPaymentOptions.java
+++ app/src/main/java/bernardo/castro/s301073235/castroActivityPaymentOptions.java
@@ -2,14 +2,51 @@ package bernardo.castro.s301073235;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
 //Bernardo Silveira dos Santos Castro, StudentID: 301073235, Section 002
 public class castroActivityPaymentOptions extends AppCompatActivity {
 
+    RadioGroup group;
+    RadioButton cash, card;
+    Button form;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.castro_activity_payment_options);
+        group = findViewById(R.id.bernardoRadioGroup);
+        cash = findViewById(R.id.bernardoCashButton);
+        card = findViewById(R.id.bernardoCardButton);
+        form = findViewById(R.id.bernardoButtonForm);
+
+        group.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+                switch (i){
+                    case R.id.bernardoCashButton:
+                        form.setVisibility(View.INVISIBLE);
+                        Toast.makeText(getApplicationContext(), getString(R.string.cash_selected), Toast.LENGTH_LONG).show();
+                        break;
+                    case R.id.bernardoCardButton:
+                        form.setVisibility(View.VISIBLE);
+                        break;
+                    default:
+                        break;
+                }
+            }
+        });
+        form.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(castroActivityPaymentOptions.this, castroActivityCardForm.class));
+            }
+        });
     }
 
 }
