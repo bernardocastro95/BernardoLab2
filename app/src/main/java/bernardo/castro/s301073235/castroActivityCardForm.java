e6898126740e6fec0156c9e7fe62114c28886b31
Bernardo Castro <bernardoc544@gmail.com>
2020-10-11 23:00:45 -0400

    Validation

--- app/src/main/java/bernardo/castro/s301073235/castroActivityCardForm.java
+++ app/src/main/java/bernardo/castro/s301073235/castroActivityCardForm.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.InputFilter;
+import android.text.Spanned;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -19,23 +21,80 @@ public class castroActivityCardForm extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.castro_activity_card_form);
-        submit = findViewById(R.id.bernardoButtonForm);
+        submit = findViewById(R.id.bernardoSubmitCardButton);
         number = findViewById(R.id.bernardoCardNumber);
         name = findViewById(R.id.bernardoCardName);
         movie = findViewById(R.id.bernardoFavoriteMovie);
         food = findViewById(R.id.bernardoFavoriteFood);
         city = findViewById(R.id.bernardoFavoriteCity);
 
-        /*submit.setOnClickListener(new View.OnClickListener() {
+        name.setFilters(new InputFilter[] {
+                new InputFilter() {
+                    public CharSequence filter(CharSequence src, int start,
+                                               int end, Spanned dst, int dstart, int dend) {
+                        if(src.equals("")){ // for backspace
+                            return src;
+                        }
+                        if(src.toString().matches("[a-zA-Z ]+")){
+                            return src;
+                        }
+                        return name.getText().toString();
+                    }
+                }
+        });
+        movie.setFilters(new InputFilter[] {
+                new InputFilter() {
+                    public CharSequence filter(CharSequence src, int start,
+                                               int end, Spanned dst, int dstart, int dend) {
+                        if(src.equals("")){ // for backspace
+                            return src;
+                        }
+                        if(src.toString().matches("[a-zA-Z ]+")){
+                            return src;
+                        }
+                        return movie.getText().toString();
+                    }
+                }
+        });
+        city.setFilters(new InputFilter[] {
+                new InputFilter() {
+                    public CharSequence filter(CharSequence src, int start,
+                                               int end, Spanned dst, int dstart, int dend) {
+                        if(src.equals("")){ // for backspace
+                            return src;
+                        }
+                        if(src.toString().matches("[a-zA-Z ]+")){
+                            return src;
+                        }
+                        return city.getText().toString();
+                    }
+                }
+        });
+        food.setFilters(new InputFilter[] {
+                new InputFilter() {
+                    public CharSequence filter(CharSequence src, int start,
+                                               int end, Spanned dst, int dstart, int dend) {
+                        if(src.equals("")){ // for backspace
+                            return src;
+                        }
+                        if(src.toString().matches("[a-zA-Z ]+")){
+                            return src;
+                        }
+                        return food.getText().toString();
+                    }
+                }
+        });
+
+        submit.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {*/
-                /*number.getText().toString();
+            public void onClick(View view) {
+                number.getText().toString();
                 name.getText().toString();
                 movie.getText().toString();
                 food.getText().toString();
-                city.getText().toString();*/
-                //Toast.makeText(getApplicationContext(), getString(R.string.payment), Toast.LENGTH_SHORT).show();
-                //startActivity(new Intent(castroActivityCardForm.this, bernardoActivity.class));
+                city.getText().toString();
+                Toast.makeText(getApplicationContext(), getString(R.string.payment), Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(castroActivityCardForm.this, bernardoActivity.class));
             }
         });
     }
